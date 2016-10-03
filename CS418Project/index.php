c1b3a60bc4d3827b111520ba64cc825502d6a12a
Trai Corte <tcorte@odu.edu>
2016-10-03 10:27:25 -0400

    Some styling for the website

--- CS418Project/index.php
+++ CS418Project/index.php
@@ -6,12 +6,65 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>UnstackingExchange</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="Header">
+            <div id="header-wrapper">
+                <div id="header-links">
+                    <a href="" id="Register">sign up</a>
+                    <a href="" id="Login">log in</a>
+                    <a href="" title="help">help</a>
+                </div>
+            </div>
+            <img src="" alt=""/>
+            <form class="FormLogin">
+                Username: <input type="text">
+                Password: <input type="password">
+                <input type="submit" value="Login">
+            </form>
+            <form class="FormRegister">
+                Email: <input type="text">
+                Username: <input type="text">
+                Password: <input type="password">
+                <input type="submit" value="Register">
+            </form>
+            <h1>Unstacking Exchange</h1>
+        </div>
+        <div id="Container">
+            <div id="Content">
+                <aside class="TQuest">
+                    <h3>Top Questions</h3>
+                    <ul>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 1</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 2</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 3</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 4</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 5</li>
+                    </ul>
+                </aside>
+                <aside class="PQuest">
+                    <h3>Previously Asked Questions</h3>
+                    <ul>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 1</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 2</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 3</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 4</li>
+                        <li>This is a really long title that will be a placeholder for the titles until functionality works number 5</li>
+                    </ul>
+                </aside>
+            </div>
+        </div>
+        <div id="Footer">
+            <aside>
+                <h4>Class Website</h4>
+            </aside>
+            <aside>
+                <h4>Team Members</h4>
+            </aside>
+        </div>
     </body>
 </html>
