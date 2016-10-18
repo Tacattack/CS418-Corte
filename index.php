dc6d4fb11cf38ee5ec84923fed6fd2073a483242
Trai Corte <tcorte@odu.edu>
2016-10-18 09:42:08 -0400

    Fixed? part 2

--- index.php
+++ index.php
@@ -9,19 +9,12 @@
             $servername = "localhost";
             $username = "root";
             $password = "";
-            $database = "myDB";
 
             // Create connection
             $conn = mysqli_connect($servername, $username, $password, $database);
 
             // Check connection
             if (!$conn) {
-                //Create the database
-                $sql = "CREATE DATABASE IF NOT EXISTS myDB";
-                if (mysqli_query($conn, $sql)) {
-                    echo "Database created successfully";
-                }else{
-                    echo "Error creating database: " . mysqli_error($conn);
                     die("Connection failed: " . mysqli_connect_error());
                 }
             }
@@ -30,8 +23,7 @@
             $sql = "CREATE TABLE IF NOT EXISTS Question (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
             questionTitle NOT NULL,
-            questionBody NOT NULL,
-            reg_date TIMESTAMP
+            questionBody NOT NULL
             )";
 
             if (mysqli_query($conn, $sql)) {
