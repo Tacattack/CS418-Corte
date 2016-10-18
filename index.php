72d4dbf82d3606cd6fc43f0d8e1f21e487efa880
Trai Corte <tcorte@odu.edu>
2016-10-18 10:36:18 -0400

    This is a little annoying now

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
-        <!--<?php require_once('Connect.php'); ?>-->
     </head>
     <body>
         <div id="Header">
