7d52b55f3abb417a30e75f4cd2e16fccb5b87f4a
Trai Corte <tcorte@odu.edu>
2016-10-18 10:33:47 -0400

    Think this fixes some issues

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php require_once('Connect.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
+        <?php require_once('Connect.php'); ?>
     </head>
     <body>
         <div id="Header">
