c183cb13f9dfaade80734ed93f7dfa4970d68c03
Trai <tcort001@odu.edu>
2016-10-18 15:02:19 -0400

    Still don't know if this will work

--- ask.php
+++ ask.php
@@ -52,6 +52,31 @@
                             </tr>
                         </table>
                         <input type="submit" value="Submit Question" style="margin: 20px 50px; float: right"/>
+                        <?PHP 
+                            $servername = "localhost";
+                            $username = "root";
+                            $password = "";
+                            $dbName = "QuestionAnswer";
+
+                            // Create connection
+                            $conn = mysqli_connect($servername, $username, $password, $dbName);
+                            // Check connection
+                            if (!$conn) {
+                                die("Connection failed: " . mysqli_connect_error());
+                            }
+
+                            $sql="INSERT INTO Questions (questionTitle, questionBody)
+                                VALUES
+                                ('$_POST[QTitle]','$_POST[QBody]')";
+
+                            if (mysqli_query($conn, $sql)) {
+                                echo "Questions Asked successfully";
+                            } else {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            }
+
+                            mysqli_close($conn);
+                        ?>
                     </form>
                 </div>
             </div>
@@ -60,31 +85,5 @@
             
         </div>
     </body>
-    
-   <?PHP 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbName = "QuestionAnswer";
-
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbName);
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        $sql="INSERT INTO Questions (questionTitle, questionBody)
-            VALUES
-            ('$_POST[QTitle]','$_POST[QBody]')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "Questions Asked successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-       
-        mysqli_close($conn);
-    ?>
 </html>
 
