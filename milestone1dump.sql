3d13a52acbdfd497a803a62418c07b8be5cd4d85
Trai Corte <tcorte@odu.edu>
2016-10-19 09:43:24 -0400

    Maybe it's a database, maybe its a mysqldump

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -20,7 +20,7 @@
 -- Current Database: `QuestionAnswer`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `QuestionAnswer` /*!40100 DEFAULT CHAR
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `QuestionAnswer` /*!40100 DEFAULT CHARA
 CTER SET latin1 */;
 
 USE `QuestionAnswer`;
@@ -34,9 +34,9 @@ DROP TABLE IF EXISTS `Questions`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `questionTitle` char(100) DEFAULT NULL,
-  `questionBody` char(250) DEFAULT NULL,
-  `answerBody` char(250) DEFAULT NULL,
+  `questionTitle` char(250) NOT NULL,
+  `questionBody` char(250) NOT NULL,
+  `answerBody` char(250) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,43 +49,15 @@ LOCK TABLES `Questions` WRITE;
 /*!40000 ALTER TABLE `Questions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Questions` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Current Database: `mysql`
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `mysql` /*!40100 DEFAULT CHARACTER SET
-latin1 */;
-
-USE `mysql`;
-
---
--- Table structure for table `columns_priv`
---
-
-DROP TABLE IF EXISTS `columns_priv`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `columns_priv` (
-  `Host` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Db` char(64) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `User` char(16) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Table_name` char(64) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Column_name` char(64) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TI
-ESTAMP,
-  `Column_priv` set('Select','Insert','Update','References') CHARACTER SET utf8
-NOT NULL DEFAULT '',
-  PRIMARY KEY (`Host`,`Db`,`User`,`Table_name`,`Column_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column privilege
-';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `columns_priv`
---
-
-LOCK TABLES `columns_priv` WRITE;
-/*!40000 ALTER TABLE `columns_priv` DISABLE KEYS */;
-/*!40000 ALTER TABLE `columns_priv` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Dump completed on 2016-10-19 13:40:20
+~
