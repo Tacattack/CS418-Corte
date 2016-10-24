c9850432fa4cf11d6dff795d4c788107a75876ab
Trai Corte <tcorte@odu.edu>
2016-10-24 09:27:47 -0400

    Maybe?

--- ask.php
+++ ask.php
@@ -75,7 +75,7 @@
                         $QuestionBody = mysqli_real_escape_string($QuestionBody);
 
                         $sql = "INSERT INTO Questions (questionTitle, questionBody)
-                            VALUES('$QuestionTitle', '$QuestionBody')";
+                            VALUES($QuestionTitle, $QuestionBody)";
 
                         if (mysqli_query($conn, $sql)) {
                             echo "New record created successfully";
