82958771ece463465992519e4d7d88e989c40c4f
Trai Corte <tcorte@odu.edu>
2016-10-24 09:54:38 -0400

    Looks like questions weren't posting. Trying something new

--- ask.php
+++ ask.php
@@ -68,11 +68,8 @@
                             die("Connection failed: " . mysqli_connect_error());
                         }
 
-                        $QuestionTitle = $_POST['QTitle'];
-                        $QuestionBody = $_POST['QBody'];
-
-                        $QuestionTitle = mysqli_real_escape_string($QuestionTitle);
-                        $QuestionBody = mysqli_real_escape_string($QuestionBody);
+                        $QuestionTitle = $conn->mysqli_real_escape_string($_POST['QTitle']);
+                        $QuestionBody = $conn->mysqli_real_escape_string($_POST['QBody']);
 
                         $sql = "INSERT INTO Questions (questionTitle, questionBody)
                             VALUES('$QuestionTitle', '$QuestionBody')";
