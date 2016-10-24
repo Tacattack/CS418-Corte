a62f4a86cb98ba35e0f9e99d6fa58d9a9d6f597a
Trai Corte <tcorte@odu.edu>
2016-10-24 09:02:28 -0400

    Forgot a line

--- ask.php
+++ ask.php
@@ -55,34 +55,36 @@
                     </form>
                     
                     <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "QuestionAnswer";
+                    if(isset($_POST["submit"])){
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "QuestionAnswer";
 
-                    // Create connection
-                    $conn = mysqli_connect($servername, $username, $password, $dbname);
-                    // Check connection
-                    if (!$conn) {
-                        die("Connection failed: " . mysqli_connect_error());
+                        // Create connection
+                        $conn = mysqli_connect($servername, $username, $password, $dbname);
+                        // Check connection
+                        if (!$conn) {
+                            die("Connection failed: " . mysqli_connect_error());
+                        }
+
+                        $QuestionTitle = $_POST['QTitle'];
+                        $QuestionBody = $_POST['QBody'];
+
+                        $QuestionTitle = mysqli_real_escape_string($QuestionTitle);
+                        $QuestionBody = mysqli_real_escape_string($QuestionBody);
+
+                        $sql = "INSERT INTO Questions (questionTitle, questionBody)
+                            VALUES('$QuestionTitle', '$QuestionBody')";
+
+                        if (mysqli_query($conn, $sql)) {
+                            echo "New record created successfully";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                        }
+
+                        mysqli_close($conn);
                     }
-
-                    $QuestionTitle = $_POST['QTitle'];
-                    $QuestionBody = $_POST['QBody'];
-
-                    $QuestionTitle = mysqli_real_escape_string($QuestionTitle);
-                    $QuestionBody = mysqli_real_escape_string($QuestionBody);
-
-                    $sql = "INSERT INTO Questions (questionTitle, questionBody)
-                        VALUES('$QuestionTitle', '$QuestionBody')";
-
-                    if (mysqli_query($conn, $sql)) {
-                        echo "New record created successfully";
-                    } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                    }
-
-                    mysqli_close($conn);
                     ?>
                 </div>
             </div>
