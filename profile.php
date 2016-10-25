5029bfc6557778b00edf717d995b8754c5a271ba
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-10-25 09:23:49 -0400

    Worked on basic user profile and uploading

---
+++ profile.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>UnstackingExchange</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
+    </head>
+    <body>
+        <div id="Header">
+            <div id="header-wrapper">
+                <div id="header-links">
+                    <a href="" id="Register">sign up</a>
+                    <a href="" id="Login">log in</a>
+                    <a href="" title="help">help</a>
+                </div>
+            </div>
+            <img src="" alt=""/>
+            <div class="Navigation">
+                <h1>
+                    <a href="index.php"><img src="images/Logo.png" class="logo"></a>
+                </h1>
+                <ul>
+                    <li><a href="#" class="active">View Questions</a></li>
+                    <li><A href="ask.php">Ask Question</a></li>
+                </ul>
+            </div>
+            <div class="Profile">
+                <h1>
+                    User Profile
+                </h1>
+                
+                <form action="PHP/Upload.php" method="post" enctype="multipart/form-data">
+                    Select a profile image:
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload Image" name="submit">
+                </form>
+                
+                
+            </div>
+        </div>
+        <div id="Footer">
+            
+        </div>
+    </body>
+</html>
+
+
