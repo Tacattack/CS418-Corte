fa20a332e121b25f6111dd2676a6551de312e5fb
Trai Corte <tcorte@odu.edu>
2016-11-14 11:57:32 -0500

    No clue at this point

--- PHP/Login.php
+++ PHP/Login.php
@@ -23,7 +23,7 @@ echo "Checking the login";
          session_register("myusername");
          $_SESSION['login_user'] = $myusername;
          echo "Session Created";
-         header("location: welcome.php");
+         header("location: redirected.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
