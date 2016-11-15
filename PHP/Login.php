f5a8ec1afef54beca95c8ac7b42f6bba87f5fc9d
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-15 09:11:05 -0500

    Start the log in!

--- PHP/Login.php
+++ PHP/Login.php
@@ -5,18 +5,18 @@ echo "Checking the login";
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
-      echo "Passing the Username and Password";
+      echo "Passing the Username and Password, ";
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      echo "Checking the DB";
+      echo "Checking the DB, ";
       $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
-      
+      echo "Count = " + $count;
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
