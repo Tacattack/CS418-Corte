bed1e98f320b5c6e10a3335fcc58f4bad775c6d3
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-15 13:24:15 -0500

    Maybe this will work or go to the moon and back because I don't have the slightest clue

--- profile.php
+++ profile.php
@@ -36,44 +36,35 @@ include ('Login.php');
                     <input type="submit" value="Upload Image" name="submit">
                 </form>
                 
-                <?php
-                $questionID = (isset($_GET["id"]) && trim($_GET["id"]) == 'QuestionView.php') ? trim($_GET["id"]) : '';
-                
-                $sql = "SELECT * FROM Questions WHERE id='".$_GET["id"] . "'";
-                $sqlA = "SELECT * FROM Answers WHERE questionID='".$_GET["id"]."' ORDER BY answerScore DESC";
-                $result = mysqli_query($conn, $sql);
-                $resultA = mysqli_query($conn, $sqlA);
-                
-
-                if (mysqli_num_rows($result) > 0)
-                {
-                    while ($row = mysqli_fetch_assoc($result))
-                    {
-                            echo "<div id=\"QuestionTitle\">";
-                            echo "<h1>" . $row["questionTitle"] . "</h1>";
-                            echo "</div>";
-
-                            echo "<div id=\"QuestionBody\">";
-                            echo "<p>";
-                            echo $row["questionBody"];
-                            echo "</p>";
-                            echo "</div>";
-                            
-                            echo "<div id=\"Answers\">";
-                            echo "<h3>Answers</h3>";
-                            echo "<ul>";
-                            if (mysqli_num_rows($resultA) > 0)
+                <h3>Asked Questions</h3>
+                    <?php
+                        $sql = "SELECT * FROM Questions ORDER BY id DESC LIMIT 5";
+                        $result = mysqli_query($conn, $sql);
+                        
+                        if (mysqli_num_rows($result) > 0)
+                        {
+                            while ($row = mysqli_fetch_assoc($result))
                             {
-                                while ($rowA = mysqli_fetch_assoc($resultA))
-                                {
-                                    echo "<li>" . "<p id=\"AnswerScore\">" . $rowA["answerScore"] . "</p>" .
-                                     "<p id=\"AnswerText\">" . $rowA["answerBody"] . "</p>" . "</li>";
-                                }
+                                echo "<div>";
+                                    echo "<div id=\"questionScore\">";
+                                        echo "<h5>" . $row["questionScore"] . "</h5>";
+                                    echo "</div>";
+                                    echo "<div id=\"questionTitleLink\">";
+                                        echo "<a href=\"QuestionView.php?id=" . $row["id"]. "\">";
+                                            echo "<h5>" . $row["questionTitle"] . "</h5>" . "</a>";
+                                    echo "</div>";
+                                echo "</div>";
                             }
-                            echo "<br />";
-                    }
-                }
-                ?>   
+                        }else {
+                            echo "<div>";
+                            echo "<h5>";
+                            echo "0 Results Found";
+                            echo "</h5>";
+                            echo "</div>";
+                        }
+                        
+                        mysqli_close($conn);
+                    ?>
             </div>
         </div>
         <div id="Footer">
