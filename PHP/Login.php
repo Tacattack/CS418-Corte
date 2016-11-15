b7babd1c0418984392d0962f1d7de36afb9c531f
Trai <tcort001@odu.edu>
2016-11-15 17:06:50 -0500

    Creating the debugs to stop the breaking

--- PHP/Login.php
+++ PHP/Login.php
@@ -8,6 +8,7 @@ if (isset($_POST('submit')))
 {
     if (empty($_POST['username']) || empty($_POST['password'])) {
         $error = "Username or Password is invalid";
+        echo $error;
     }
     else
     {
@@ -20,17 +21,23 @@ if (isset($_POST('submit')))
         $password = stripslashes($password);
         $username = mysql_real_escape_string($username);
         $password = mysql_real_escape_string($password);
-
+        
+        echo "I did some password things";
         // SQL query to fetch information of registerd users and finds user match.
         $query = mysqli_query("select * from UserProfile where password='$password' AND username='$username'");
         $rows = mysqli_num_rows($query);
+        echo "I created the query";
         if ($rows == 1) {
             $_SESSION['login_user']=$username; // Initializing Session
             header("location: profile.php"); // Redirecting To Other Page
         } else {
             $error = "Username or Password is invalid";
+            echo $error;
         }
     }
+}
+else{
+    echo "The first if is broke";
 }
 ?>
 
