0f1fe7eb3c3383fd20f8000dc1d6a8083c8bc2d5
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-15 09:47:13 -0500

    Removed comments, on to the next trial!

--- PHP/Login.php
+++ PHP/Login.php
@@ -1,22 +1,19 @@
 <?php
 include("Connect.php");
 session_start();
-echo "Checking the login";
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
-      echo "Passing the Username and Password, ";
+
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      echo "Checking the DB, ";
+ 
       $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
-      echo "Count = ";
-      echo $count;
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
