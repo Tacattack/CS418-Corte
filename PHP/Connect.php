f43fbfb7eeac2cf680cee029e1c64b3eeffa039d
Trai <tcort001@odu.edu>
2016-11-15 16:33:05 -0500

    Took away the broke

--- PHP/Connect.php
+++ PHP/Connect.php
@@ -11,38 +11,5 @@
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
-            
-            //This will begin the login in process and create a session once the user logs in
-            $ID = $_POST['username'];
-            $PASSWORD = $_POST['password'];
-            
-            function SignIn()
-            {
-                session_start();
-                if(!empty($_POST['user']))
-                {
-                    $query = mysqli_query("SELECT * FROM UserProfile where username = '$_POST[username]' AND password = '$_POST[password]'");
-                    $row = mysqli_fetch_array($query);
-                    
-                    if (!empty($row['username'] AND !empty($row['password'])))
-                    {
-                        $_SESSION['username'] = $row['password'];
-                        echo "signed in";
-                    }
-                    else
-                    {
-                        echo "Password or username wrong";
-                    }
-                }
-                else
-                {
-                    echo "this be breaking";
-                }
-            }
-            
-            if (isset($_POST['submit']))
-            {
-                SignIn();
-            }
 ?>
 
