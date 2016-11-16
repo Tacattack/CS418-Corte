f441d44ac441528b0c994093de7dbe6549f6986f
Trai Corte <tcorte@odu.edu>
2016-11-16 09:43:22 -0500

    Debugs fa dayz

--- PHP/Login.php
+++ PHP/Login.php
@@ -1,36 +1,36 @@
 <?php
-    echo "I run before the include \n";
+    echo "I run before the include";
     include ("Connect.php");
-    echo "I run after the include \n";
+    echo "I run after the include";
     
-    echo $dbName. "\n";
-    echo "The username is: ".$_POST('username')."\n";
-    echo "The password is: ".$_POST('password')."\n";
-    $USRNM = $_POST('username');
-    $PSSWRD = $_POST('password');
+    echo $dbNam;
+    echo "The username is: ".$_POST('LoginUsername');
+    echo "The password is: ".$_POST('LoginPassword');
+    $USRNM = $_POST('LoginUsername');
+    $PSSWRD = $_POST('LoginPassword');
     
-    echo $USRNM."\n";
-    echo $PSSWRD."\n";
+    echo $USRNM;
+    echo $PSSWRD;
     
     //Protecting username from MYSQL injection
     $USRNM = stripslashes($USRNM);
     $USRNM = mysqli_real_escape_string($USRNM);
-    echo "username protected \n";
+    echo "username protected";
     //Protecting password from MYSL injection
     $PSSWRD = stripslashes($PSSWRD);
     $PSSWRD = mysqli_real_escape_string($PSSWRD);
-    echo "password protected \n";
+    echo "password protected";
     //comparing username and password to database
     $sqlLogin = "SELECT * FROM UserProfile WHERE username='$USRNM' and password='$PSSWRD'";
     $resultLogin = mysqli_query($sqlLogin);
-    echo "query created \n";
+    echo "query created";
     //Couting the table row
     $count = mysqli_num_rows($result);
     echo $count;
     //if $result matched username and password, table row must be 1
     if ($count == 1)
     {
-        echo "Count == 1 \n";
+        echo "Count == 1";
         //Register the username and password then redirect it to the profile page
         session_register("USRNM");
         session_register("PSSWRD");
