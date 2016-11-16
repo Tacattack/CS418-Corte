d68cf4a8b86b4fa46dbcf850b7d886acd19021fa
Trai Corte <tcorte@odu.edu>
2016-11-16 11:27:16 -0500

    I really don't want to fail this class

--- PHP/Login.php
+++ PHP/Login.php
@@ -14,42 +14,38 @@
     echo $psswrd;
     echo $db;
     
+    echo "The username is: ".$_POST('LoginUsername');
+    echo "The password is: ".$_POST('LoginPassword');
 
-    if (isset($_POST['Login']))
+    $USRNM = $_POST('LoginUsername');
+    $PSSWRD = $_POST('LoginPassword');
+
+    echo $USRNM;
+    echo $PSSWRD;
+
+    //Protecting username from MYSQL injection
+    $USRNM = stripslashes($USRNM);
+    $USRNM = mysqli_real_escape_string($USRNM);
+    echo "username protected";
+    //Protecting password from MYSL injection
+    $PSSWRD = stripslashes($PSSWRD);
+    $PSSWRD = mysqli_real_escape_string($PSSWRD);
+    echo "password protected";
+    //comparing username and password to database
+    $sqlLogin = "SELECT * FROM UserProfile WHERE username='".$USRNM."' AND password='".$PSSWRD."' LIMIT 1";
+    $resultLogin = mysqli_query($sqlLogin);
+    echo "query created";
+    //if $result matched username and password, table row must be 1
+    if (mysqli_num_rows($resultLogin) == 1)
     {
-        echo "The username is: ".$_POST('LoginUsername');
-        echo "The password is: ".$_POST('LoginPassword');
-        
-        $USRNM = $_POST('LoginUsername');
-        $PSSWRD = $_POST('LoginPassword');
-        
-        echo $USRNM;
-        echo $PSSWRD;
-        
-        //Protecting username from MYSQL injection
-        $USRNM = stripslashes($USRNM);
-        $USRNM = mysqli_real_escape_string($USRNM);
-        echo "username protected";
-        //Protecting password from MYSL injection
-        $PSSWRD = stripslashes($PSSWRD);
-        $PSSWRD = mysqli_real_escape_string($PSSWRD);
-        echo "password protected";
-        //comparing username and password to database
-        $sqlLogin = "SELECT * FROM UserProfile WHERE username='".$USRNM."' AND password='".$PSSWRD."' LIMIT 1";
-        $resultLogin = mysqli_query($sqlLogin);
-        echo "query created";
-        //if $result matched username and password, table row must be 1
-        if (mysqli_num_rows($resultLogin) == 1)
-        {
-            //Register the username and password then redirect it to the profile page
-            echo "You have successfully logged in";
-            exit();
-        }
-        else
-        {
-            echo "Wrong username and password";
-            exit();
-        }
+        //Register the username and password then redirect it to the profile page
+        echo "You have successfully logged in";
+        exit();
+    }
+    else
+    {
+        echo "Wrong username and password";
+        exit();
     }
     
     echo "I either skipped the if or completed it no probs";
