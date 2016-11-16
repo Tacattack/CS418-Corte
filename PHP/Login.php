fbbc5c3d3ab06b188dc3135be439a7a220a16c46
Trai Corte <tcorte@odu.edu>
2016-11-16 11:22:31 -0500

    Somethings

--- PHP/Login.php
+++ PHP/Login.php
@@ -1,49 +1,54 @@
 <?php
-    echo "I run before the include \r\n";
-    include ("Connect.php");
-    echo "I run after the include \r\n";
+    echo "I run before the connection";
+    $host = "localhost";
+    $user = "root";
+    $passwrd = "";
+    $db = "QuestionAnswer";
     
-    echo $dbName."\r\n";
+    mysqli_connect($host, $user, $pass);
+    mysqli_select_db($db);
+    echo "I run after the connection";
+    
+    echo $host;
+    echo $user;
+    echo $psswrd;
+    echo $db;
     
 
-    if (isset($_POST['LoginUsername']))
+    if (isset($_POST['Login']))
     {
-        echo "The username is: ".$_POST('LoginUsername')."\r\n";
-        echo "The password is: ".$_POST('LoginPassword')."\r\n";
+        echo "The username is: ".$_POST('LoginUsername');
+        echo "The password is: ".$_POST('LoginPassword');
         
         $USRNM = $_POST('LoginUsername');
         $PSSWRD = $_POST('LoginPassword');
         
-        echo $USRNM."\r\n";
-        echo $PSSWRD."\r\n";
+        echo $USRNM;
+        echo $PSSWRD;
         
         //Protecting username from MYSQL injection
         $USRNM = stripslashes($USRNM);
         $USRNM = mysqli_real_escape_string($USRNM);
-        echo "username protected \r\n";
+        echo "username protected";
         //Protecting password from MYSL injection
         $PSSWRD = stripslashes($PSSWRD);
         $PSSWRD = mysqli_real_escape_string($PSSWRD);
-        echo "password protected \r\n";
+        echo "password protected";
         //comparing username and password to database
         $sqlLogin = "SELECT * FROM UserProfile WHERE username='".$USRNM."' AND password='".$PSSWRD."' LIMIT 1";
         $resultLogin = mysqli_query($sqlLogin);
-        echo "query created \r\n";
-        //Couting the table row
-        $count = mysqli_num_rows($result);
-        echo $count;
+        echo "query created";
         //if $result matched username and password, table row must be 1
-        if ($count == 1)
+        if (mysqli_num_rows($resultLogin) == 1)
         {
-            echo "Count == 1 \r\n";
             //Register the username and password then redirect it to the profile page
-            session_register("USRNM");
-            session_register("PSSWRD");
-            header("location: profile.php");
+            echo "You have successfully logged in";
+            exit();
         }
         else
         {
             echo "Wrong username and password";
+            exit();
         }
     }
     
--- index.php
+++ index.php
@@ -1,56 +1,6 @@
 <?php
 //require_once("redirected.php");
     require_once("PHP/Connect.php");
-    
-    echo "I run before the include \r\n";
-    include ("Connect.php");
-    echo "I run after the include \r\n";
-    
-    echo $dbName."\r\n";
-    
-
-    if (isset($_POST['Login']))
-    {
-        echo "The username is: ".$_POST('LoginUsername')."\r\n";
-        echo "The password is: ".$_POST('LoginPassword')."\r\n";
-        
-        $USRNM = $_POST('LoginUsername');
-        $PSSWRD = $_POST('LoginPassword');
-        
-        echo $USRNM."\r\n";
-        echo $PSSWRD."\r\n";
-        
-        //Protecting username from MYSQL injection
-        $USRNM = stripslashes($USRNM);
-        $USRNM = mysqli_real_escape_string($USRNM);
-        echo "username protected \r\n";
-        //Protecting password from MYSL injection
-        $PSSWRD = stripslashes($PSSWRD);
-        $PSSWRD = mysqli_real_escape_string($PSSWRD);
-        echo "password protected \r\n";
-        //comparing username and password to database
-        $sqlLogin = "SELECT * FROM UserProfile WHERE username='".$USRNM."' AND password='".$PSSWRD."' LIMIT 1";
-        $resultLogin = mysqli_query($sqlLogin);
-        echo "query created \r\n";
-        //Couting the table row
-        $count = mysqli_num_rows($result);
-        echo $count;
-        //if $result matched username and password, table row must be 1
-        if ($count == 1)
-        {
-            echo "Count == 1 \r\n";
-            //Register the username and password then redirect it to the profile page
-            session_register("USRNM");
-            session_register("PSSWRD");
-            header("location: profile.php");
-        }
-        else
-        {
-            echo "Wrong username and password";
-        }
-    }
-    
-    echo "I either skipped the if or completed it no probs";
 ?>
 <!DOCTYPE html>
 <html>
@@ -69,7 +19,7 @@
                     <a href="" title="Help">help</a>
                 </div>
             </div>
-            <form method="post" class="FormLogin" action="">
+            <form method="post" class="FormLogin" action="PHP/Login.php">
                 Username: <input type="text" name="LoginUsername">
                 Password: <input type="password" name="LoginPassword"> 
                <input type="submit" name="Login" value="Login">
