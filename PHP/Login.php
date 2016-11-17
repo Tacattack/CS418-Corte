523b907de1af722f722e575c0a50b1d67098dd6d
Trai Corte <tcorte@odu.edu>
2016-11-17 10:08:51 -0500

    The difference between [] and () was the cause this whole time

--- PHP/Login.php
+++ PHP/Login.php
@@ -14,34 +14,34 @@
     $USRNM = $_POST['LoginUsername'];
     $PSSWRD = $_POST['LoginPassword'];
 
-    echo $USRNM;
-    echo $PSSWRD;
+    echo "| ".$USRNM." |";
+    echo "| ".$PSSWRD." |";
 
     //Protecting username from MYSQL injection
     $USRNM = stripslashes($USRNM);
     $USRNM = mysqli_real_escape_string($USRNM);
-    echo "username protected";
+    echo "| username protected |";
     //Protecting password from MYSL injection
     $PSSWRD = stripslashes($PSSWRD);
     $PSSWRD = mysqli_real_escape_string($PSSWRD);
-    echo "password protected";
+    echo "| password protected |";
     //comparing username and password to database
     $sqlLogin = "SELECT * FROM UserProfile WHERE username='".$USRNM."' AND password='".$PSSWRD."' LIMIT 1";
     $resultLogin = mysqli_query($conn, $sqlLogin);
-    echo "query created";
+    echo "| query created |";
     //if $result matched username and password, table row must be 1
     if (mysqli_num_rows($resultLogin) == 1)
     {
         //Register the username and password then redirect it to the profile page
-        echo "You have successfully logged in";
+        echo "| You have successfully logged in |";
         exit();
     }
     else
     {
-        echo "Wrong username and password";
+        echo "| Wrong username and password |";
         exit();
     }
     
-    echo "I either skipped the if or completed it no probs";
+    echo "| If statement skipped |";
 ?>
 
