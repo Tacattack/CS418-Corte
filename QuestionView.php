12ce185c4bc41dcb934b829bd93c015663c115ab
Trai Corte <tcort001@gmail.com>
2016-11-18 16:01:58 -0500

    together a force almost 3 times as big as the empire

--- QuestionView.php
+++ QuestionView.php
@@ -99,6 +99,11 @@ session_start();
                                         if (isset($_SESSION["USER"]))
                                         {
                                             echo "<li><p id=\"AnswerScore\">".$rowA["answerScore"]."</p><p id=\"AnswerText\">".$rowA["answerBody"]."</p></li>";
+                                            echo "<li><form><table>";
+                                            echo "<tr><td>type=\"submit\" name=\"Like\" value=\"I Like\"></td>".$rowA["answerBody"]."<td></td></tr>";
+                                            echo "<tr><td>type=\"submit\" name=\"upVote\" value=\"+\"></td><td>type=\"submit\" name=\"downVote\" value=\"-\"></td>"
+                                            .$rowA["answerScore"].$rowA["answerPoster"]."<td></td></tr>";
+                                            echo "</li></form></table>";
                                         }
                                         else
                                         {
