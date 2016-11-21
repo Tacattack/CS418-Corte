aaeceff67c39f1e3ef3fc500f3ce2d0b9878b8cf
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 14:34:50 -0500

    Still not the code you're looking for.

--- questions.php
+++ questions.php
@@ -74,7 +74,7 @@ session_start();
                                     if($_SESSION["USERLEVEL"] == 1)
                                     {
                                     echo "<div id=\"questionDelete\">";
-                                        echo "<form action=\"\" method=\"post\"> <input type=\"hidden\" name=\"Delete\ value=\"\">"
+                                        echo "<td><input type=\"submit\" name=\"Delete\ value=\"EXTERMINATE\"></td>"
                                     . ""
                                     ."<?php $sql =\"DELETE * FROM Questions WHERE id =$row \" ?>";
                                     }
