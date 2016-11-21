c9bd647b702ef994be7758e2e166db1b96d93e5f
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 18:15:07 -0500

    Brunelle must love our tears and suffering.

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -9,6 +9,7 @@ include ("questions.php");
 echo "Getting Parameter that was stored via session". " | ";
 //$result = $_GET['id'];
 $results =$_SESSION["CurrentRow"];
+echo "This is the results" .$results. " | ";
 echo "<p>This is the ID: " .$_SESSION["CurrentRow"]. "</p>" . " | ";
 //mysql_query("DELETE FROM Questions WHERE id='$results'");
 
