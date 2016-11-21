a1010c91393ffb2c1fe28b05be907007777baa6d
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 18:11:49 -0500

    All the programmers were crying fa la la la

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -1,13 +1,17 @@
 <?php
+echo "Session Starting";
 session_start();
+
+echo "Including files";
 include ("Connect.php");
 include ("questions.php");
 
+echo "Getting Parameter that was stored via session";
 //$result = $_GET['id'];
 echo "<p>This is the ID: " . $_SESSION["CurrentRow"]. "</p>";
 //$results =$_SESSION["CurrentRow"];
 //mysql_query("DELETE FROM Questions WHERE id='$results'");
 
-header("Location: ../questions.php");
+//header("Location: ../questions.php");
 ?>
 
