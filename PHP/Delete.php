fac84ba4a980a1a57a5df4da11c6e63cb5d2f716
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 18:02:43 -0500

    Made an oopsie causing stuff to not work.

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -4,7 +4,8 @@ include ("Connect.php");
 include ("questions.php");
 
 //$result = $_GET['id'];
-echo "<p>This is the ID: " . $result . "</p>";
+echo "<p>This is the ID: " . $_SESSION["CurrentRow"]. "</p>";
+$result =$_SESSION["CurrentRow"];
 mysql_query("DELETE FROM Questions WHERE id='$result'");
 
 //header("Location: ../questions.php");
