581a2ae37ca6cb1575ecfd28d491b13abe9617ce
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 17:13:26 -0500

    Debuging the deletion.

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -3,6 +3,7 @@ include ("Connect.php");
 include ("questions.php");
 
 $id = $_GET['id'];
+echo "This is the ID: " . $id;
 mysql_query("DELETE from purchase WHERE id='$id'");
 
 header("Location: ../questions.php");
