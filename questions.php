88dddcc149257ff57ba8e66e1863217611676ef6
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 17:55:11 -0500

    Maybe this won't exterminate me.

--- questions.php
+++ questions.php
@@ -58,7 +58,7 @@ session_start();
                     <?php
                         $sql = "SELECT * FROM Questions ORDER BY id DESC";
                         $result = mysqli_query($conn, $sql);
-                        
+                        header('Location: PHP/Delete.php?var=result');
                         if (mysqli_num_rows($result) > 0)
                         {
                             while ($row = mysqli_fetch_assoc($result))
