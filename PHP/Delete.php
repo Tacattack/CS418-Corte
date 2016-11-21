ffec4b5e9bd14a302ffd0422b0e26526a6763610
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 18:06:30 -0500

    Really starting to get pissed that this isn't working.

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -6,7 +6,7 @@ include ("questions.php");
 //$result = $_GET['id'];
 echo "<p>This is the ID: " . $_SESSION["CurrentRow"]. "</p>";
 $results =$_SESSION["CurrentRow"];
-mysql_query("DELETE FROM Questions WHERE id='$results'");
+//mysql_query("DELETE FROM Questions WHERE id='$results'");
 
 header("Location: ../questions.php");
 ?>
