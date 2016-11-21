3b51f918b885107c048d118e9683003cfedfb815
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-21 18:36:56 -0500

    See change broke stuff.

--- PHP/Delete.php
+++ PHP/Delete.php
@@ -9,7 +9,7 @@ include ("questions.php");
 echo "Getting Parameter that was stored via session". " | ";
 $result = $_SESSION['CurrentRow'];
 //echo "<p>This is the ID: " .$_SESSION["CurrentRow"]. "</p>" . " | ";
-mysql_query("DELETE FROM Questions WHERE id=$result)";
+mysql_query("DELETE FROM Questions WHERE id=$result");
 
 header("Location: ../questions.php");
 ?>
