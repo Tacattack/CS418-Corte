5b7f3176ed2ae192dcedab7c851538222dfcacc1
Nicholas Benfield <nicholas.j.benfield@gmail.com>
2016-11-22 14:33:20 -0500

    Come on work for me!

--- PHP/Freezing.php
+++ PHP/Freezing.php
@@ -3,7 +3,9 @@ include ("Connect.php");
 include ("questions.php");
 
 $status = intval($_GET['freeze']);
-$id = intval($_GET["delete"]);
+$id = intval($_GET["data-value"]);
+
+echo $id;
 
 $result = "UPDATE Questions SET questionFrozen = $status WHERE id=$id";
 $sqlresult = mysqli_query($conn,$result);
