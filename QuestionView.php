ce5d3d2a8c3ff312b146cee2a708628696e0af62
Trai Corte <tcorte@odu.edu>
2016-11-22 10:11:52 -0500

    pagination try

--- QuestionView.php
+++ QuestionView.php
@@ -98,7 +98,89 @@ session_start();
                             echo "<div id=\"Answers\">";
                             echo "<h3>Answers</h3>";
                             echo "<ul>";
-                            if (mysqli_num_rows($resultA) > 0)
+                            
+                            try {
+
+                                // Find out how many items are in the table
+                                $total = $dbh->query('
+                                    SELECT
+                                        COUNT(*)
+                                    FROM
+                                        Answers
+                                ')->fetchColumn();
+
+                                // How many items to list per page
+                                $limit = 20;
+
+                                // How many pages will there be
+                                $pages = ceil($total / $limit);
+
+                                // What page are we currently on?
+                                $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+                                    'options' => array(
+                                        'default'   => 1,
+                                        'min_range' => 1,
+                                    ),
+                                )));
+
+                                // Calculate the offset for the query
+                                $offset = ($page - 1)  * $limit;
+
+                                // Some information to display to the user
+                                $start = $offset + 1;
+                                $end = min(($offset + $limit), $total);
+
+                                // The "back" link
+                                $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+                                // The "forward" link
+                                $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+                                // Display the paging information
+                                echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+                                // Prepare the paged query
+                                $stmt = $dbh->prepare('
+                                    SELECT * FROM Answers ORDER BY id LIMIT :limit OFFSET :offset');
+
+                                // Bind the query params
+                                $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+                                $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+                                $stmt->execute();
+
+                                // Do we have any results?
+                                if ($stmt->rowCount() > 0) {
+                                    // Define how we want to fetch the results
+                                    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                                    $iterator = new IteratorIterator($stmt);
+
+                                    // Display the results
+                                    foreach ($iterator as $row) {
+                                        if (isset($_SESSION["USER"]))
+                                    {
+                                        echo "<li><form action=\"\" method=\"post\"><input type=\"hidden\" name=\"likeIt\"><table>";
+                                        echo "<tr><td><input type=\"submit\" name=\"Like\" value=\"I Like\"></td><td>".$rowA["answerBody"]."</td></tr>";
+                                        echo "<tr><td><input type=\"submit\" name=\"upVote\" value=\"+\">&nbsp".$rowA["answerScore"]."&nbsp<input type=\"submit\" name=\"downVote\" value=\"-\">"
+                                            ."</td><td> posted by: ".$rowA["answerPoster"]."</td></tr>";
+                                        echo "</table></form></li>";
+                                    }
+                                    else
+                                    {
+                                        echo "<li><form><table>";
+                                        echo "<tr><td>".$rowA["answerScore"]."</td><td>".$rowA["answerBody"]."</td></tr>";
+                                        echo "<tr><td> posted by: ".$rowA["answerPoster"]."</td></tr>";
+                                        echo "</table></form></li>";
+                                    }
+                                    }
+
+                                } else {
+                                    echo '<p>No results could be displayed.</p>';
+                                }
+
+                            } catch (Exception $e) {
+                                echo '<p>', $e->getMessage(), '</p>';
+                            }
+                            /*if (mysqli_num_rows($resultA) > 0)
                             {
                                 while ($rowA = mysqli_fetch_assoc($resultA))
                                 {
@@ -118,22 +200,11 @@ session_start();
                                         echo "</table></form></li>";
                                     }
                                 }
-                            }
+                            }*/
                         }
                         echo "<br />";
                     }
                     echo "</ul>";
-                    
-                    if( $page > 0 ) {
-                        $last = $page - 2;
-                        echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a> |";
-                        echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
-                     }else if( $page == 0 ) {
-                        echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
-                     }else if( $left_rec < $rec_limit ) {
-                        $last = $page - 2;
-                        echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a>";
-                     }
                     
                     if (isset($_SESSION["USER"]))
                     {
