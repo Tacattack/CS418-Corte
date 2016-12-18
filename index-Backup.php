acd3a56513129ef6814cd0baa7dfa85243f87b07
Trai Corte <tcort001@gmail.com>
2016-12-18 14:49:37 -0500

    New Look Who Dis?

---
+++ index-Backup.php
@@ -0,0 +1,121 @@
+<?php
+//require_once("redirected.php");
+require_once("PHP/Connect.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UnstackingExchange</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
+    </head>
+    <body>
+        <div id="Header">
+                <?PHP
+                    if (isset($_SESSION["USER"]))
+                    {
+                        echo "<form class=\"FormLogin\" action=\"PHP/Logout.php\" method=\"post\">";
+                        echo "<b>Welcome: <a href=\"profile.php?id=\"".$_SESSION["USERID"]."\">".$_SESSION["USER"]."</a></b>";
+                        echo "&nbsp&nbsp&nbsp";
+                        echo "<input type=\"submit\" name=\"submit\" value=\"Logout\">";
+                        echo "</form>";
+                    }
+                    else
+                    {
+                        echo "<form class=\"FormLogin\" action=\"PHP/Login.php\" method=\"post\">";
+                        echo "Username: <input type=\"text\" name=\"LoginUsername\">";
+                        echo "Password: <input type=\"password\" name=\"LoginPassword\">"; 
+                        echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
+                        echo "</form>";
+                        echo "<a href=\"register.php\">Register</a>";
+                    }
+                ?>
+            <div class="Navigation">
+                <h1>
+                    <a href="index.php"><img src="images/Logo.png" class="logo"></a>
+                </h1>
+                <ul>
+                    <li><a href="questions.php">View Questions</a></li>
+                    <li><a href="ask.php">Ask Question</a></li>
+                    <li><a href="help.php">Help</a></li>
+                   <?php
+                        if($_SESSION["USERLEVEL"] == 1)
+                        {
+                            echo "<li><a href =\"users.php\" class=\"active\">Users</a></li>";
+                        }
+                        ?>
+                                        <li><form action="" method="post">
+                        <input type="radio" name="search" value="Users"/> Users
+                        <input type="radio" name="search" value="Questions"/> Questions
+                        <input type="text" name="search" placeholder="Search..."></li>
+                </ul>
+            </div>
+        </div>
+        <div id="Container">
+            <div id="Content">
+                <aside class="TQuest">
+                    <h3>Top Questions</h3>
+                    <?php
+                        $sqlT = "SELECT * FROM Questions ORDER BY questionScore DESC LIMIT 5";
+                        $resultT = mysqli_query($conn, $sqlT);
+                        
+                        if (mysqli_num_rows($resultT) > 0)
+                        {
+                            while ($rowT = mysqli_fetch_assoc($resultT))
+                            {
+                                echo "<div>";
+                                    echo "<div id=\"questionScore\">";
+                                        echo "<h5>" . $rowT["questionScore"] . "</h5>";
+                                    echo "</div>";
+                                    echo "<div id=\"questionTitleLink\">";
+                                        echo "<a href=\"QuestionView.php?id=" . $rowT["id"]. "\">";
+                                            echo "<h5>" . $rowT["questionTitle"] . "</h5>" . "</a>";
+                                    echo "</div>";
+                                echo "</div>";
+                            }
+                        }else {
+                            echo "<div>";
+                            echo "<h5>";
+                            echo "0 Results Found";
+                            echo "</h5>";
+                            echo "</div>";
+                        }
+                    ?>
+                </aside>
+                <aside class="PQuest">
+                    <h3>Previously Asked Questions</h3>
+                    <?php
+                        $sql = "SELECT * FROM Questions ORDER BY id DESC LIMIT 5";
+                        $result = mysqli_query($conn, $sql);
+                        
+                        if (mysqli_num_rows($result) > 0)
+                        {
+                            while ($row = mysqli_fetch_assoc($result))
+                            {
+                                echo "<div>";
+                                    echo "<div id=\"questionScore\">";
+                                        echo "<h5>" . $row["questionScore"] . "</h5>";
+                                    echo "</div>";
+                                    echo "<div id=\"questionTitleLink\">";
+                                        echo "<a href=\"QuestionView.php?id=" . $row["id"]. "\">";
+                                            echo "<h5>" . $row["questionTitle"] . "</h5>" . "</a>";
+                                    echo "</div>";
+                                echo "</div>";
+                            }
+                        }else {
+                            echo "<div>";
+                            echo "<h5>";
+                            echo "0 Results Found";
+                            echo "</h5>";
+                            echo "</div>";
+                        }
+                        
+                        mysqli_close($conn);
+                    ?>
+                </aside>
+            </div>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -4,118 +4,145 @@ require_once("PHP/Connect.php");
 session_start();
 ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>UnstackingExchange</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
-    </head>
-    <body>
-        <div id="Header">
-                <?PHP
-                    if (isset($_SESSION["USER"]))
-                    {
-                        echo "<form class=\"FormLogin\" action=\"PHP/Logout.php\" method=\"post\">";
-                        echo "<b>Welcome: <a href=\"profile.php?id=\"".$_SESSION["USERID"]."\">".$_SESSION["USER"]."</a></b>";
-                        echo "&nbsp&nbsp&nbsp";
-                        echo "<input type=\"submit\" name=\"submit\" value=\"Logout\">";
-                        echo "</form>";
-                    }
-                    else
-                    {
-                        echo "<form class=\"FormLogin\" action=\"PHP/Login.php\" method=\"post\">";
-                        echo "Username: <input type=\"text\" name=\"LoginUsername\">";
-                        echo "Password: <input type=\"password\" name=\"LoginPassword\">"; 
-                        echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
-                        echo "</form>";
-                        echo "<a href=\"register.php\">Register</a>";
-                    }
-                ?>
-            <div class="Navigation">
-                <h1>
-                    <a href="index.php"><img src="images/Logo.png" class="logo"></a>
-                </h1>
-                <ul>
-                    <li><a href="questions.php">View Questions</a></li>
-                    <li><a href="ask.php">Ask Question</a></li>
-                    <li><a href="help.php">Help</a></li>
-                   <?php
-                        if($_SESSION["USERLEVEL"] == 1)
-                        {
-                            echo "<li><a href =\"users.php\" class=\"active\">Users</a></li>";
-                        }
-                        ?>
-                                        <li><form action="" method="post">
-                        <input type="radio" name="search" value="Users"/> Users
-                        <input type="radio" name="search" value="Questions"/> Questions
-                        <input type="text" name="search" placeholder="Search..."></li>
-                </ul>
-            </div>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Unstacking Exchange</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrapDist/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Unstacking Exchange</a>
         </div>
-        <div id="Container">
-            <div id="Content">
-                <aside class="TQuest">
-                    <h3>Top Questions</h3>
-                    <?php
-                        $sqlT = "SELECT * FROM Questions ORDER BY questionScore DESC LIMIT 5";
-                        $resultT = mysqli_query($conn, $sqlT);
-                        
-                        if (mysqli_num_rows($resultT) > 0)
-                        {
-                            while ($rowT = mysqli_fetch_assoc($resultT))
-                            {
-                                echo "<div>";
-                                    echo "<div id=\"questionScore\">";
-                                        echo "<h5>" . $rowT["questionScore"] . "</h5>";
-                                    echo "</div>";
-                                    echo "<div id=\"questionTitleLink\">";
-                                        echo "<a href=\"QuestionView.php?id=" . $rowT["id"]. "\">";
-                                            echo "<h5>" . $rowT["questionTitle"] . "</h5>" . "</a>";
-                                    echo "</div>";
-                                echo "</div>";
-                            }
-                        }else {
-                            echo "<div>";
-                            echo "<h5>";
-                            echo "0 Results Found";
-                            echo "</h5>";
-                            echo "</div>";
-                        }
-                    ?>
-                </aside>
-                <aside class="PQuest">
-                    <h3>Previously Asked Questions</h3>
-                    <?php
-                        $sql = "SELECT * FROM Questions ORDER BY id DESC LIMIT 5";
-                        $result = mysqli_query($conn, $sql);
-                        
-                        if (mysqli_num_rows($result) > 0)
-                        {
-                            while ($row = mysqli_fetch_assoc($result))
-                            {
-                                echo "<div>";
-                                    echo "<div id=\"questionScore\">";
-                                        echo "<h5>" . $row["questionScore"] . "</h5>";
-                                    echo "</div>";
-                                    echo "<div id=\"questionTitleLink\">";
-                                        echo "<a href=\"QuestionView.php?id=" . $row["id"]. "\">";
-                                            echo "<h5>" . $row["questionTitle"] . "</h5>" . "</a>";
-                                    echo "</div>";
-                                echo "</div>";
-                            }
-                        }else {
-                            echo "<div>";
-                            echo "<h5>";
-                            echo "0 Results Found";
-                            echo "</h5>";
-                            echo "</div>";
-                        }
-                        
-                        mysqli_close($conn);
-                    ?>
-                </aside>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Username" class="form-control">
             </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+            <br /><a href="register.html">Need an account? Register Here</a>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Unstacking Exchange</h1>
+        <p>Unstacking your game dev needs</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">View Questions &raquo;</a>
+        	<a class="btn btn-primary btn-lg" href="#" role="button">Ask Question &raquo;</a>
+    		<a class="btn btn-primary btn-lg" href="#" role="button">Help &raquo;</a>
+    	</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-6">
+          <h2>Previous Questions</h2>
+          <?php
+            $sql = "SELECT * FROM Questions ORDER BY id DESC LIMIT 5";
+            $result = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($result) > 0)
+            {
+                while ($row = mysqli_fetch_assoc($result))
+                {
+                    echo "<div>";
+                        echo "<div id=\"questionScore\">";
+                            echo "<h5>" . $row["questionScore"] . "</h5>";
+                        echo "</div>";
+                        echo "<div id=\"questionTitleLink\">";
+                            echo "<a href=\"QuestionView.php?id=" . $row["id"]. "\">";
+                                echo "<h5>" . $row["questionTitle"] . "</h5>" . "</a>";
+                        echo "</div>";
+                    echo "</div>";
+                }
+            }else {
+                echo "<div>";
+                echo "<h5>";
+                echo "0 Results Found";
+                echo "</h5>";
+                echo "</div>";
+            }
+        ?>
         </div>
-    </body>
+        <div class="col-md-6">
+          <h2>Top Questions</h2>
+          <?php
+            $sqlT = "SELECT * FROM Questions ORDER BY questionScore DESC LIMIT 5";
+            $resultT = mysqli_query($conn, $sqlT);
+            
+            if (mysqli_num_rows($resultT) > 0)
+            {
+                while ($rowT = mysqli_fetch_assoc($resultT))
+                {
+                    echo "<div>";
+                        echo "<div id=\"questionScore\">";
+                            echo "<h5>" . $rowT["questionScore"] . "</h5>";
+                        echo "</div>";
+                        echo "<div id=\"questionTitleLink\">";
+                            echo "<a href=\"QuestionView.php?id=" . $rowT["id"]. "\">";
+                                echo "<h5>" . $rowT["questionTitle"] . "</h5>" . "</a>";
+                        echo "</div>";
+                    echo "</div>";
+                }
+            }else {
+                echo "<div>";
+                echo "<h5>";
+                echo "0 Results Found";
+                echo "</h5>";
+                echo "</div>";
+            }
+
+            mysqli_close($conn);
+        ?>
+        </div>
+      </div>
+
+      <hr>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrapDist/dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
