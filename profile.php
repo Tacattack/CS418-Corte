b2d294022e7b4b789c817381b21e9a4e75b1f93b
Trai <tcort001@odu.edu>
2017-03-11 18:46:05 -0500

    Profile page on new template

--- profile.php
+++ profile.php
@@ -1,19 +1,88 @@
 <?php
-require("PHP/Connect.php");
+require_once("PHP/Connect.php");
 session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>UnstackingExchange</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
-    </head>
-    <body>
-        <div id="Header">
-            <?PHP
-                    if (isset($_SESSION["USER"]))
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Unstacking Exchange</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrapDist/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Unstacking Exchange</a>
+        </div>
+          <?PHP
+                   if (isset($_SESSION["USER"]))
+                    {
+                        echo "<form class=\"FormLogin\" action=\"PHP/Logout.php\" method=\"post\">";
+                        echo "<img src=\"\" style=\"height:35px; width:35px;\">";
+                        echo "&nbsp&nbsp&nbsp";
+                        echo "<b>Welcome: <a href=\"profile.php?id=\"".$_SESSION["USERID"]."\">".$_SESSION["USER"]."</a></b>";
+                        echo "&nbsp&nbsp&nbsp";
+                        echo "<input type=\"submit\" name=\"submit\" value=\"Logout\">";
+                        echo "</form>";
+                    }
+                    else
+                    {
+                        echo "<form class=\"navbar-form navbar-right\" action=\"PHP/Login.php\" method=\"post\">";
+                        echo "<div class=\"form-group\">";
+                        echo "<input type=\"text\" placeholder=\"Username\" class=\"form-control\">";
+                        echo "</div>";
+                        echo "<div class=\"form-group\">";
+                        echo "<input type=\"password\" placeholder=\"Password\" class=\"form-control\">";
+                        echo "</div>";
+                        echo "<button type=\"submit\" class=\"btn btn-success\">Sign in</button>";
+                        echo "<br /><a href=\"register.html\">Need an account? Register Here</a>";
+                        echo "</form>";
+                    }
+                ?>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Unstacking Exchange</h1>
+        <p>Unstacking your game dev needs</p>
+        <p><a class="btn btn-primary btn-lg" href="questions.php" role="button">View Questions &raquo;</a>
+        	<a class="btn btn-primary btn-lg" href="ask.php" role="button">Ask Question &raquo;</a>
+    		<a class="btn btn-primary btn-lg" href="help.php" role="button">Help &raquo;</a>
+    	</p>
+      </div>
+    </div>
+    <?PHP
+                   /* if (isset($_SESSION["USER"]))
                     {
 
                         //Checking the User's level of Access
@@ -51,32 +120,10 @@ session_start();
                         echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
                         echo "</form>";
                         echo "<a href=\"register.php\">Register</a>";
-                    }
+                    }*/
                 ?>
-            <img src="" alt=""/>
-            <div class="Navigation">
-                <h1>
-                    <a href="index.php"><img src="images/Logo.png" class="logo"></a>
-                </h1>
-                <ul>
-                    <li><a href="questions.php">View Questions</a></li>
-                    <li><a href="ask.php">Ask Question</a></li>
-                    <li><a href="help.php">Help</a></li>
-
-                    <?php
-                        if($UserLevel == "Admin")
-                        {
-                            echo "<li><a href =\"users.php\" class=\"active\">Users</a></li>";
-                        }
-                        ?>
-                                        <li><form action="" method="post">
-                        <input type="radio" name="search" value="Users"/> Users
-                        <input type="radio" name="search" value="Questions"/> Questions
-                        <input type="text" name="search" placeholder="Search..."></li>
-                </ul>
-            </div>
-            <div id="Container">
-                <?php
+    <div class="container">
+      <?php
                     $UserID = (isset($_GET["id"]) && trim($_GET["id"]) == 'profile.php') ? trim($_GET["id"]) : '';
                     $sqlU = "SELECT * FROM UserProfile WHERE id='".$_GET["id"]."'";
                     $resultU = mysqli_query($conn, $sqlU);
@@ -135,12 +182,14 @@ session_start();
 
                     mysqli_close($conn);
                 ?>
-            </div>
-        </div>
-        <div id="Footer">
-            
-        </div>
-    </body>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrapDist/dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
-
-
