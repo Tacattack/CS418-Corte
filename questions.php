b8182e5ca55bf1674a3a9bf0354b562ae0d96960
Trai <tcort001@odu.edu>
2017-03-12 15:40:56 -0400

    Fuck pagination. Revert

--- questions.php
+++ questions.php
@@ -1,17 +1,6 @@
 <?php
 require_once("PHP/Connect.php");
-require_once ("PHP/paginator.class.php");
 session_start();
-
-$conn = new mysqli('localhost', 'root', '', 'QuestionAnswer');
-$limit = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
-$page = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
-$links = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
-$query = "SELECT * FROM Questions ORDER BY id DESC";
- 
-$Paginator  = new Paginator( $conn, $query );
- 
-$results = $Paginator->getData( $page, $limit );
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -104,7 +93,13 @@ $results = $Paginator->getData( $page, $limit );
       <div class="row">
         <div class="col-md-8">
           <?php
-            for ($i = 0; $i < count($result->data); $i++) :
+            $sql = "SELECT * FROM Questions ORDER BY id DESC";
+            $result = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($result) > 0)
+            {
+                while ($row = mysqli_fetch_assoc($result))
+                {
                         echo "<div class=\"row\">";
                         echo "<div class=\"col-xs-12 col-sm-12\">";
                             echo "<div class=\"row\">";
@@ -138,11 +133,16 @@ $results = $Paginator->getData( $page, $limit );
                         echo "</div>";
                     echo "</div>";
                     echo "<hr>";
-                    
-                    endfor;
+                    }
+                }else {
+                    echo "<div>";
+                    echo "<h5>";
+                    echo "0 Results Found";
+                    echo "</h5>";
+                    echo "</div>";
+                }          
+                mysqli_close($conn);
             ?>
-            
-            <?php echo $Paginator->createLinks($links, 'pagination pagination-sm'); ?>
         </div>
           <div class="col-md-4">
               
