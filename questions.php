06feef3d8cba62589a41a6885174ab6f33bbefc1
Trai <tcort001@odu.edu>
2017-03-12 12:34:58 -0400

    Another pagination attempt

--- questions.php
+++ questions.php
@@ -93,14 +93,74 @@ session_start();
       <div class="row">
         <div class="col-md-8">
           <?php
-            $sql = "SELECT * FROM Questions ORDER BY id DESC";
-            $result = mysqli_query($conn, $sql);
-            
-            if (mysqli_num_rows($result) > 0)
-            {
-                while ($row = mysqli_fetch_assoc($result))
-                {
-                        echo "<div class=\"row\">";
+            try {
+
+    // Find out how many items are in the table
+    $total = $dbh->query('
+        SELECT
+            COUNT(id)
+        FROM
+            Questions
+    ')->fetchColumn();
+
+    // How many items to list per page
+    $limit = 20;
+
+    // How many pages will there be
+    $pages = ceil($total / $limit);
+
+    // What page are we currently on?
+    $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+        'options' => array(
+            'default'   => 1,
+            'min_range' => 1,
+        ),
+    )));
+
+    // Calculate the offset for the query
+    $offset = ($page - 1)  * $limit;
+
+    // Some information to display to the user
+    $start = $offset + 1;
+    $end = min(($offset + $limit), $total);
+
+    // The "back" link
+    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+    // The "forward" link
+    $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+    // Display the paging information
+    echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+    // Prepare the paged query
+    $stmt = $dbh->prepare('
+        SELECT
+            *
+        FROM
+            Questions
+        ORDER BY
+            id
+        LIMIT
+            :limit
+        OFFSET
+            :offset
+    ');
+
+    // Bind the query params
+    $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $stmt->execute();
+
+    // Do we have any results?
+    if ($stmt->rowCount() > 0) {
+        // Define how we want to fetch the results
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $iterator = new IteratorIterator($stmt);
+
+        // Display the results
+        foreach ($iterator as $row) {
+            echo "<div class=\"row\">";
                         echo "<div class=\"col-xs-12 col-sm-12\">";
                             echo "<div class=\"row\">";
                                 echo "<div class=\"col-xs-2 col-sm-2 col-md-2\">";
@@ -133,14 +193,15 @@ session_start();
                         echo "</div>";
                     echo "</div>";
                     echo "<hr>";
-                    }
-                }else {
-                    echo "<div>";
-                    echo "<h5>";
-                    echo "0 Results Found";
-                    echo "</h5>";
-                    echo "</div>";
-                }          
+        }
+
+    } else {
+        echo '<p>No results could be displayed.</p>';
+    }
+
+} catch (Exception $e) {
+    echo '<p>', $e->getMessage(), '</p>';
+}         
                 mysqli_close($conn);
             ?>
         </div>
