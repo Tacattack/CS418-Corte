7e5263e579654171cf37589a82b37afdfdff16f8
Trai <tcort001@odu.edu>
2017-03-12 23:02:04 -0400

    We try again

--- questions.php
+++ questions.php
@@ -104,7 +104,7 @@ $limit = 10;
 if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };  
 $start_from = ($page-1) * $limit;  
   
-$sql = "SELECT * FROM Questions ORDER BY title ASC LIMIT $start_from, $limit";  
+$sql = "SELECT * FROM Questions ORDER BY questionTitle ASC LIMIT $start_from, $limit";  
 $rs_result = mysql_query ($sql);  
 ?>  
 <table class="table table-bordered table-striped">  
@@ -121,7 +121,7 @@ while ($row = mysql_fetch_assoc($rs_result)) {
     echo "<tr>" . $row["questionTitle"]. "</td>";  
     echo "<td>".$row["questionPoster"]."</td>";  
     echo "</tr>";
-};
+}
 ?>  
 </tbody>  
 </table>  
