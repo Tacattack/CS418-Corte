ec6b659719f7a0033bf919f3fc58d15e545d1a24
Trai <tcort001@odu.edu>
2017-03-20 14:11:13 -0400

    Checking to see if images are actually being put on the DB

---
+++ imageCheck.php
@@ -0,0 +1,145 @@
+<?php
+require_once("PHP/Connect.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Unstacking Exchange</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrapDist/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+    <link href="register.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Unstacking Exchange</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <?PHP
+                   if (isset($_SESSION["USER"]))
+                    {
+                        echo "<form class=\"FormLogin navbar-form navbar-right\" action=\"PHP/Logout.php\" method=\"post\">";
+                        echo "<img src=\"\" style=\"height:35px; width:35px;\">";
+                        echo "&nbsp&nbsp&nbsp";
+                        echo "<b>Welcome: <a href=\"profile.php?id=".$_SESSION["USERID"]."\">".$_SESSION["USER"]."</a></b>";
+                        echo "&nbsp&nbsp&nbsp";
+                        echo "<input type=\"submit\" class=\"btn btn-success\" name=\"submit\" value=\"Logout\">";
+                        echo "</form>";
+                    }
+                    else
+                    {
+                        echo "<form class=\"navbar-form navbar-right\" action=\"PHP/Login.php\" method=\"post\">";
+                        echo "<div class=\"form-group\">";
+                        echo "<input type=\"text\" name=\"LoginUsername\" placeholder=\"Username\" class=\"form-control\">";
+                        echo "</div>";
+                        echo "<div class=\"form-group\">";
+                        echo "<input type=\"password\" name=\"LoginPassword\" placeholder=\"Password\" class=\"form-control\">";
+                        echo "</div>";
+                        echo "<input type=\"submit\" name=\"submit\"class=\"btn btn-success\" value=\"Sign In\">";
+                        echo "<br /><a href=\"register.php\">Need an account? Register Here</a>";
+                        echo "</form>";
+                    }
+                ?>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+        <form class="form-signin" name="Register" method="post" action="">
+        <h2 class="form-signin-heading">Register Here</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="rEmail" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="username" name="rUsername" id="inputUsername" class="form-control" placeholder="Username" required>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="rPassword" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Register">
+      </form>
+        
+        <?php
+                    $qryP = "SELECT * FROM UserPictures";
+                    $resultP = mysqli_query($qryP, $conn);
+                    if (mysqli_num_rows($resultP) > 0)
+                    {
+                        while ($row = mysqli_fetch_array($resultP))
+                        {
+                            echo '<img style="height:50px width:50px" src="data:image/jpeg;base64,'.\base64_encode($row['picture']).'"/>';
+                        }
+                    }
+                        echo "<form action=\"\" method=\"post\" enctype=\"multipart/form-data\">";
+                        echo "Select a profile image:";
+                        echo "<input type=\"file\" name=\"image\" id=\"fileToUpload\">";
+                        echo "<input type=\"submit\" value=\"Upload Image\" name=\"submit\">";
+                        echo  "</form>";
+                        
+                        if(isset($_POST['submit']))
+                        {   
+                            if (getimagesize($_FILES['image']['tmp_name']) == false)
+                            {
+                                echo "Please select an image.";
+                            }
+                            else
+                            {
+                                $pictureUploader = $_SESSION["USER"];
+                                $image = addslashes($_FILES['image']['tmp_name']);
+                                $name = addslashes($_FILES['image']['name']);
+                                $imageA = file_get_contents($image);
+                                $imageE = \base64_encode($imageE);
+                                saveimage($name, $image, $pictureUploader);
+                                
+                                $qry = "insert into UserPictures (user, pictureName, picture)
+                                    VALUES ('{$pictureUploader}','{$name}','{$image}')";
+
+                                $result = mysqli_query($qry, $conn);
+                                if($result)
+                                {
+                                    echo "<br />Image Uploaded";
+                                }
+                                else
+                                {
+                                    echo "<br />Image Not Uploaded";
+                                }                                
+                            }
+                        }
+                        
+                        mysqli_close($conn);
+            ?>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrapDist/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
