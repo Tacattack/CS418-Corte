c5eb60c0a926b0b53662407388279a3fa17a9526
Trai <tcort001@odu.edu>
2017-03-20 13:52:08 -0400

    Have to break it down into parts to find the error

--- PHP/Upload.php
+++ PHP/Upload.php
@@ -14,12 +14,12 @@ if(isset($_POST['submit']))
         $image = addslashes($_FILES['image']['tmp_name']);
         $name = addslashes($_FILES['image']['name']);
         $image = file_get_contents($image);
-        $image = base64_encode($image);
+        $image = \base64_encode($image);
         saveimage($name, $image, $pictureUploader);
     }
 }
 
-function saveimage($name, $image, $pictureUploader)
+/*function saveimage($name, $image, $pictureUploader)
 {
     $qry = "insert into UserPictures (user, pictureName, picture)
             VALUES ('{$pictureUploader}','{$name}','{$image}')";
@@ -29,7 +29,7 @@ function saveimage($name, $image, $pictureUploader)
         echo "<br />Image Uploaded";
     else
         echo "<br />Image Not Uploaded";
-}
+}*/
 
 header("Location: ../profile.php?id=".$_SESSION["USERID"]);
 ?>
