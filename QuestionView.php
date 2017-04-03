682f774534d78a69e60f20f134776a9521acbaac
Trai Corte <tcorte@odu.edu>
2017-04-03 13:59:03 -0400

    Not so dark anymore

--- QuestionView.php
+++ QuestionView.php
@@ -352,7 +352,7 @@ session_start();
                                                     $AVoteType = 1;
                                                     if ($rowA["bestAnswer"] == 1)
                                                     {
-                                                        echo '<div class="col-md-8" style="background-color:#1E8449;">';
+                                                        echo '<div class="col-md-8" style="background-color:#28B463;">';
                                                     }
                                                     else if ($rowA["bestAnswer"] == 0)
                                                     {
@@ -380,7 +380,7 @@ session_start();
                                                     $AVoteType = -1;
                                                     if ($rowA["bestAnswer"] == 1)
                                                     {
-                                                        echo '<div class="col-md-8" style="background-color:#1E8449;">';
+                                                        echo '<div class="col-md-8" style="background-color:#28B463;">';
                                                     }
                                                     else if ($rowA["bestAnswer"] == 0)
                                                     {
@@ -410,7 +410,7 @@ session_start();
                                     {
                                         if ($rowA["bestAnswer"] == 1)
                                         {
-                                            echo '<div class="col-md-8" style="background-color:#1E8449;">';
+                                            echo '<div class="col-md-8" style="background-color:#28B463;">';
                                         }
                                         else if ($rowA["bestAnswer"] == 0)
                                         {
